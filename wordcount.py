a1f7380e5791ed85709ac92bfe4d4aff60a660af
Hackbright Student <info@hackbrightacademy.com>
2014-02-10 17:33:08 -0800

    Not quite there, doesn't sort by both value and key

--- wordcount.py
+++ wordcount.py
@@ -1,6 +1,16 @@
 # import a file
 from sys import argv
 
+def goofy_compare(s1,s2):
+
+    if s1 < s2:
+        return 1
+    elif s1 == s2:
+        return 0
+    elif s1 > s2:
+        return -1
+
+
 print argv
 if len(argv) != 2:
     print "Please enter a filname on the command line"
@@ -12,6 +22,7 @@ else:
     print filename
 
     twain = open(filename)
+    charstostrip = "!@#$%^&*()_+=-~`,./;\'\[\]\{\}\":?><"    # todo -- figure out an elegant way to to this -- all characters except the ascii set
     twain_dict = {}
     count = 0
     for line in twain:
@@ -20,16 +31,27 @@ else:
         local_list = lc_line.split()
         for i in local_list:
             # if entry does not exist (boolean), create key and set to 1
+            i = i.strip(charstostrip)
             if twain_dict.get(i) == None:
                 twain_dict[i] = 1
             # else, increment the value
             else:
                 twain_dict[i] += 1
 
-    #print "there are %d lines" % count           print twain_dict
+
+    for w in sorted(twain_dict, key=twain_dict.get, reverse=True):
+  #  for w in sorted(twain_dict.keys(), cmp=goofy_compare, reverse=True):
+       print "%s \t\t appears \t\t %d times" % (w, twain_dict[w])
+
+
+"""
 
     for key, value in twain_dict.iteritems():
-        print "%s appears %d times" % (key, value)
+        if value == 1:
+            print "%s \t appears \t\t%d time" % (key, value)
+        else:
+            print "%s \t appears \t\t%d times" % (key, value)
+"""
 
 
 
