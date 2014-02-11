eda60983be16be44a88a4294bd023806ad709a20
Hackbright Student <info@hackbrightacademy.com>
2014-02-11 12:35:23 -0800

    Working version -- uses a second dictionary

--- wordcount.py
+++ wordcount.py
@@ -1,21 +1,11 @@
 # import a file
 from sys import argv
 
-def goofy_compare(s1,s2):
-
-    if s1 < s2:
-        return 1
-    elif s1 == s2:
-        return 0
-    elif s1 > s2:
-        return -1
-
-
 print argv
 if len(argv) != 2:
     print "Please enter a filname on the command line"
 
-# Sanitization needed -- check for existence of file
+#TODO Sanitization needed -- check for existence of file
 
 else:
     filename = argv[1]
@@ -38,21 +28,38 @@ else:
             else:
                 twain_dict[i] += 1
 
+# initialize a new dictionary
+    twain_2_dict = {}
 
-    for w in sorted(twain_dict, key=twain_dict.get, reverse=True):
-  #  for w in sorted(twain_dict.keys(), cmp=goofy_compare, reverse=True):
-       print "%s \t\t appears \t\t %d times" % (w, twain_dict[w])
-
-
-"""
-
+# go through each key value pair in the original dictionary
     for key, value in twain_dict.iteritems():
-        if value == 1:
-            print "%s \t appears \t\t%d time" % (key, value)
+
+    # if, in the new dictionary, the value of the old key-value pair does not exist as a KEY, 
+        # initialize that value as a key, and initialize a list with former key as value.
+        if not twain_2_dict.get(value, False):
+            twain_2_dict[value]=[key]
+            # print "in False loop"
+            # print twain_2_dict
+        
+
+    # if, iin the new dictionary, the value of the old key-value pair DOES exist as a key, 
+        # append the list
+
         else:
-            print "%s \t appears \t\t%d times" % (key, value)
-"""
+            twain_2_dict[value].append(key)
+            # print "in True loop"
+            # print twain_2_dict
+
+# Iterate through all the values (lists) and sort them alphabetically
+# PRINT STUFF
+
+    for key, value in twain_2_dict.iteritems():
+        value.sort()
+        for i in range(len(value)):
+            print "%s \t\t appears \t\t%s times" % (value[i],key)
+#TODO treat one item separately.  
+        
+
+   
 
 
-
-    # we know the output of iterating over lines is a string
